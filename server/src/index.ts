47997926330ee659d4088ee6617ab9ca7af5f02e
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2025-03-18 20:09:50 +0530

    Added cookie-pasrser

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser"; // Add this import
 import userRoutes from "./routes/user.routes.ts";
 import { createCheckoutSession } from "./controllers/stripe.controller.ts";
 import emailRoutes from "./routes/email.routes.ts";
@@ -12,16 +13,18 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(express.json());
+app.use(cookieParser()); // Add this line
 
 app.set("trust proxy", 1);
 
+// Your CORS setup looks good with the added headers
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://quick-doc-drab.vercel.app"],
-    credentials: true, // Required for cookies to be sent
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
-
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie", "Cookie"],
+    exposedHeaders: ['Set-Cookie'],
     optionsSuccessStatus: 200,
   })
 );
